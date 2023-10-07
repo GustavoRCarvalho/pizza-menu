8b46de999b71ee984dd629b0534d63ed5274c954
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-10-07 00:26:32 -0300

    fix:  remove coments from removeAndShift function

--- src/components/Common/utils.js
+++ src/components/Common/utils.js
@@ -4,19 +4,15 @@ export function createArray(n, obj) {
 
 export function removeAndShift(array, indexToRemove, initialObject) {
   if (indexToRemove < 0 || indexToRemove >= array.length) {
-    // Verifique se o índice é válido
     return array
   }
 
-  // Defina o nome do elemento no índice especificado como uma string vazia
   array[indexToRemove] = initialObject
 
-  // Movimente os elementos à direita do índice especificado para preencher o espaço vago
   for (let i = indexToRemove + 1; i < array.length; i++) {
     array[i - 1] = array[i]
   }
 
-  // Remova o último elemento, que agora é duplicado
   array.pop()
   array.push(initialObject)
 
